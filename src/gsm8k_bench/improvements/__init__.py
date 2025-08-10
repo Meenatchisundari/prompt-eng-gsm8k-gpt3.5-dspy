8959fa46cc5ce9e0d48906543588e2d4061e8543
Meenatchi Sundari Muthirulappan <85951411+Meenatchisundari@users.noreply.github.com>
2025-08-10 14:54:17 +0100

    Create __init__.py

---
+++ src/gsm8k_bench/improvements/__init__.py
@@ -0,0 +1,15 @@
+"""
+Advanced and improved prompting techniques.
+"""
+
+from .enhanced_prolog import EnhancedPrologModule
+from .calculator_augmented import CalculatorAugmentedModule
+from .verification_chain import VerificationChainModule
+from .ensemble import WeightedEnsembleModule
+
+__all__ = [
+    'EnhancedPrologModule',
+    'CalculatorAugmentedModule',
+    'VerificationChainModule', 
+    'WeightedEnsembleModule'
+]
